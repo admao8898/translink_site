9f42cea05cb691b3f976defd2529073bf284d26a
chairman98 <admao8898@gmail.com>
2020-02-04 14:25:38 -0800

    Comment Out NextBus Test

--- TranslinkSite/TestCases/NextBusTest.cs
+++ TranslinkSite/TestCases/NextBusTest.cs
@@ -8,12 +8,12 @@ namespace TranslinkSite.TestCases
 {
     public class NextBusTest : UITestFixture
     {
-        //Next Bus ~ "NB"
-        [TestCase("R5"), Order(1)]
-        public void TextViewNB(string busroute)
-        {
-            NextBusPage nextBus = new NextBusPage(driver);
-            nextBus.TextViewNB(busroute);
-        }
+        ////Next Bus ~ "NB"
+        //[TestCase("R5"), Order(1)]
+        //public void TextViewNB(string busroute)
+        //{
+        //    NextBusPage nextBus = new NextBusPage(driver);
+        //    nextBus.TextViewNB(busroute);
+        //}
     }
 }
