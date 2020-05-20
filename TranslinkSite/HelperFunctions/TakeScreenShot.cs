bca551fd7142d30a37fbe7606c30dfa803287fe5
chairman98 <admao8898@gmail.com>
2020-05-19 19:03:08 -0700

    Add Intentional Test Fails

--- TranslinkSite/HelperFunctions/TakeScreenShot.cs
+++ TranslinkSite/HelperFunctions/TakeScreenShot.cs
@@ -19,6 +19,7 @@ namespace TranslinkSite.HelperFunctions
         public void GetScreenShot(IWebDriver driver)
         {
             var screenshot = driver.TakeScreenshot();
+            // testcontext class https://github.com/nunit/docs/wiki/TestContext
             string testMethodName = TestContext.CurrentContext.Test.MethodName + "_Failed_";
             string fileName = testMethodName + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") + ".png";
             string screenshotFile = Path.Combine(Environment.CurrentDirectory, fileName);
