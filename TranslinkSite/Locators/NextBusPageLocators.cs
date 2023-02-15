af839dc67242769783b50173ba0da5994a524bb2
David M <admao8898@gmail.com>
2023-02-14 23:42:47 -0800

    Update NextBusPageLocators.cs

--- TranslinkSite/Locators/NextBusPageLocators.cs
+++ TranslinkSite/Locators/NextBusPageLocators.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,38 @@ using System.Threading.Tasks;
 
 namespace TranslinkSite.Locators
 {
-    internal class NextBusPageLocators
+    public class NextBusPageLocators
     {
+        public readonly string nextBusURL = "https://nb.translink.ca";
+
+        public static readonly By NextBusField = By.Name("NextBusSearchTerm");
+        public static readonly By FindNB_Button = By.XPath("//button[contains(text(),'Find my next bus')]");
+        public static readonly By UseCurrentLocationButton = By.Name("useCurrentLocation");
+        public static readonly By SubmitNextBusButton = By.Id("MainContent_linkSearch");
+        public static readonly By SettingsTab = By.LinkText("Settings");
+        public static readonly By ClockTime = By.XPath("//*[@value='clockTime']");
+        public static readonly By CountDown = By.XPath("//*[@value='countDown']");
+        public static readonly By MapView = By.CssSelector("a.toggleTextMapView"); //Toggle for text view as well
+
+        public static readonly By RefreshPage = By.Id("refresh_tab");
+
+        public static readonly By BrowseRoutesContainer = By.XPath("(//*[text()='Browse all bus routes'])[2]");
+        public static readonly By Route8 = By.XPath("//*[text()='8 Fraser / Downtown']");
+        public static readonly By Route8TopDirection = By.XPath("(//*[text()='To Fraser'])[2]");
+        public static readonly By Route8Stop = By.XPath("(//*[text()='E Broadway at Kingsway'])[2]");
+
+        public static readonly By RouteR2 = By.XPath("//*[text()='R2 Marine Dr']");
+        public static readonly By RouteR2TopDirection = By.XPath("(//*[text()='To Marine Dr To Phibbs Exch'])[2]");
+        public static readonly By RouteR2Stop = By.XPath("(//*[text()='Marine Dr at Capilano Rd'])[2]");
+
+
+        public static readonly By RouteTopDestination = By.XPath("(//*[@class='InfoCard indexLinkInfoCardTheme layoutItemContent indexLink'])[1]");
+        public static readonly By RouteBottomDestination = By.XPath("(//*[@class='InfoCard indexLinkInfoCardTheme layoutItemContent indexLink'])[2]");
+        public static readonly By SecondStop = By.XPath("//*/article[2]");
+        public static readonly By TryNewNBLink = By.LinkText("Try the new Next Bus");
+
+        public readonly string nextBusPageTitle = "Next Bus is a quick way to look up departure, real time, " +
+            "or scheduled times for a specific bus stop and bus route.";
+        public readonly string nextBusPageTitleFailMsg = "Next bus Page Title Missing";
     }
 }
